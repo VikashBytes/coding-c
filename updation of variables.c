41c0ee545ce9c2f3769a68d7690e07362f8adbec
VikashBytes <vikashkelly@gmail.com>
2024-09-19 16:09:55 +0530

    added updation of variables.c

---
+++ updation of variables.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(){
+    printf("vikash\n");
+
+   return 0;
+}
--- variables.c
+++ variables.c
@@ -3,7 +3,12 @@ int main(){
     printf("vikash\n");
 
     int x;
-    x = 5;
+    x = 50;
+    printf("%d",x);
+    x = 100;
+    printf("\n%d",x);
+    x = 4;
+    printf("\n");
     printf("%d",x);
     return 0;
 }
