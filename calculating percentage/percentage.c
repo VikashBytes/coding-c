9eba61a95a4861d531bb05e35cbe18672181f7fe
VikashBytes <vikashkelly@gmail.com>
2024-09-19 22:41:05 +0530

    calculating percentage

---
+++ calculating percentage/percentage.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    float x1 = 90; //x1 can be physics
+    float x2 = 91; //x2 can be chemistry
+    float x3 = 92; //x3 can be maths
+    float x4 = 93; //x4 can be english
+    float x5 = 94; //x5 can be hindi
+    float percent = (x1 + x2 + x3  + x4 + x5)/5;
+    printf("percentage = %f",percent);
+    return 0;
+    //change the marks and run each time
+}
