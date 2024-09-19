5463e2412e5e171d6938468d8d7c29c850697f2f
VikashBytes <vikashkelly@gmail.com>
2024-09-19 23:22:59 +0530

    percentage of 4 subject out of 40 marks

---
+++ percentage of 4 subject out of 40 marks/percentageof4subject.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    float x1 = 30; //x1 can be physics
+    float x2 = 35; //x2 can be chemistry
+    float x3 = 25; //x3 can be maths
+    float x4 = 40; //x4 can be english
+    float total = (x1 + x2 + x3 + x4); //total marks
+    printf("\n");
+    float percentage = (total/160)*100; //apply formulae
+    printf("your percentage = %f",percentage);
+    return 0;
+
+}
