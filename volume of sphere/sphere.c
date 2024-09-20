ce8ffc97d74c36496270254b9afa7ac487a62d60
VikashBytes <vikashkelly@gmail.com>
2024-09-20 08:29:58 +0530

    taking input from user

--- volume of sphere/sphere.c
+++ volume of sphere/sphere.c
@@ -2,10 +2,10 @@
 int main(){
 
 float radius;
-printf("Enter your radius :"); //enter your radius using printf
+printf("Enter radiuas of sphere :"); //enter your radius using printf
 scanf("%f",&radius);
-float pi = 3.145; //value of pi
-float volume = (radius*radius*radius*pi)/3; //apply formulae
+float pi = 3.14; //value of pi
+float volume = (radius*radius*radius*pi*4)/3; //apply formulae
 printf("Volume of sphere is : %f",volume); //print output
 return 0;   
 }
