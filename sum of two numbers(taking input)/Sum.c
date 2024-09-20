c71670c309cf77b8af79408021888f7240814631
VikashBytes <vikashkelly@gmail.com>
2024-09-20 11:00:58 +0530

    sum of two numbers(taking input)

---
+++ sum of two numbers(taking input)/Sum.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int x, y, z;
+    printf("Enter the value of X :");
+    scanf("%d",&x);
+    printf("enter the value of y :");
+    scanf("%d",&y);
+    z = x + y;
+    printf("sum : %d",z);
+    return 0;
+}
