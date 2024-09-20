3099c1007dfb2b69d05ae3df6200b3cae41ae031
VikashBytes <vikashkelly@gmail.com>
2024-09-20 18:55:42 +0530

    taking input

--- float to int and int to float/floatToInt.c
+++ float to int and int to float/floatToInt.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 int main(){
 float x;
+printf("Enter a decimal number");
 scanf("%f",&x);
 int y;
 y = x;
 float z = x -y;
-printf("%f",z);
+printf("your fraction part of  %f is : %f",x, z);
 return 0;
 }
