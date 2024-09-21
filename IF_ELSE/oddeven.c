e01e020fe7f28660055ac6a3257aa50eb63d3297
VikashBytes <vikashkelly@gmail.com>
2024-09-21 23:31:50 +0530

    modified

--- IF_ELSE/oddeven.c
+++ IF_ELSE/oddeven.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 int main(){
+    // Enter Number code !
 int n;
 
 printf("enter a number : ");
 scanf("%d",&n);
-
+//even condition!
 if(n%2==0){
 printf("Even Number");
 }
+//odd condition!
 if (n%2!=0)
 {
     printf("Odd number");
