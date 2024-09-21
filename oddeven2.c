a0274579752eac042448f67c183b7735886ed8e5
vikash <vikashkelly@gmail.com>
2024-09-22 00:07:55 +0530

    revision

---
+++ oddeven2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+int n;
+printf("Enter the number : ");
+scanf("%d",&n);
+if (n%2==0)
+{
+    printf("Even Number");
+}
+else
+{
+    printf("odd Number");
+}
+
+
+    return 0;
+}
