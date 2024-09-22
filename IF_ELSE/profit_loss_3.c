359253c9d0ba086a698416d3881fe1796b95afe1
vikash <vikashkelly@gmail.com>
2024-09-22 21:14:39 +0530

    important

--- IF_ELSE/profit_loss_3.c
+++ IF_ELSE/profit_loss_3.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+//: If cost price and selling price of an item is input through the keyboard, write a program to determine whether the seller has made profit or incurred loss. Also determine how much profit he made or loss he incurred. 
 int main(){
 int cp, sp, profit, loss;
 printf("Enter CP : ");
