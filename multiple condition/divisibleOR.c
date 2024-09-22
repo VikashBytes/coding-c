5e5778d1252a4c2434d6271a65a45f6c7ffc5145
vikash <vikashkelly@gmail.com>
2024-09-22 23:17:15 +0530

    update

--- multiple condition/divisibleOR.c
+++ multiple condition/divisibleOR.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
-//Take positive integer input and tell if it
-//is divisible by 5 or 3.
-int main(){
+// Take positive integer input and tell if it
+// is divisible by 5 or 3.
+int main()
+{
     int n;
     printf("Enter integer : ");
-    scanf("%d",&n);
-    if (n%5==0 || n%3==0)
+    scanf("%d", &n);
+    if (n % 5 == 0 || n % 3 == 0)
     {
-        printf(" ya, it is divisible : %d",n);
+        printf(" ya, it is divisible : %d", n);
     }
     else
     {
         printf("no, it is not diviible : ");
     }
-    
+
     return 0;
 }
