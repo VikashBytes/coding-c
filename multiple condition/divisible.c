afdd00442aa239aa7a9c95bc125a310080745495
vikash <vikashkelly@gmail.com>
2024-09-22 22:26:04 +0530

    it is divisible by 5 and 3
    Take positive integer input and tell if it
    is divisible by 5 and 3.

---
+++ multiple condition/divisible.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+//Take positive integer input and tell if it
+//is divisible by 5 and 3.
+int main(){
+    int n;
+    printf("Enter integer : ");
+    scanf("%d",&n);
+    if (n%5==0 && n%3==0)
+    {
+        printf(" ya, it is divisible : %d",n);
+    }
+    else
+    {
+        printf("no, it is not diviible : ");
+    }
+    
+    return 0;
+}
