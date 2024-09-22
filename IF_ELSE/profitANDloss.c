fe0bf77e0809dbdc2d8f51b7cfaf61ab00e3d117
vikash <vikashkelly@gmail.com>
2024-09-22 10:40:33 +0530

    profit and loss

---
+++ IF_ELSE/profitANDloss.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+int main(){
+int cp, sp, profit, loss;
+printf("Enter CP : ");
+scanf("%d",&cp);
+
+printf("Enter SP : ");
+scanf("%d",&sp);
+
+if (sp>cp)
+{
+  printf("wow, Profit : ");
+}
+if (cp>sp)
+{
+  printf("loss");
+}
+if (sp==cp)
+{
+    printf("no profit, no loss");
+}
+
+printf("\n");
+if (sp>cp)
+{
+   profit = sp - cp;
+}
+printf("you have made profit of : %d",profit);
+
+printf("\n");
+if (cp>sp)
+{
+    loss = cp - sp;
+}
+printf("you have made losss of : %d",loss);
+
+    return 0;
+}
