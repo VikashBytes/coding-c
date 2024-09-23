a3411d647025ba75b9a79fd20973b3307f67c8c0
vikash <vikashkelly@gmail.com>
2024-09-24 00:40:19 +0530

    good morning

---
+++ IF_ELSE/GoodMorning.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+int n;
+printf("enter the secret code");
+scanf("%d",&n);
+if (n>=2)
+{
+   printf("good morning");
+}
+else
+{
+    printf("good evening");
+}
+
+    return 0;
+}
