a5c42bcb400a6bf005cb743983bfd0e372ee22cb
vikash <vikashkelly@gmail.com>
2024-09-23 23:27:40 +0530

    update

--- multiple condition/smallestNO.c
+++ multiple condition/smallestNO.c
@@ -1,27 +1,28 @@
 #include <stdio.h>
-int main(){
+int main()
+{
     int ram, shyam, ajay;
     printf("Enter the age odf ram");
-    scanf("%d",&ram);
+    scanf("%d", &ram);
 
     printf("Enter the age of shyam");
-    scanf("%d",&shyam);
+    scanf("%d", &shyam);
 
     printf("enter the age of ajay");
-    scanf("%d",&ajay);
-if (ram<shyam && ram<ajay)
-{
-   printf("hey, ram is younger");
-}
-if (shyam<ram && shyam<ajay)
-{
-    printf("hey, shyam is younger");
-}
+    scanf("%d", &ajay);
+    if (ram < shyam && ram < ajay)
+    {
+        printf("hey, ram is younger");
+    }
+    if (shyam < ram && shyam < ajay)
+    {
+        printf("hey, shyam is younger");
+    }
 
-else
-{
-    printf("hey, ajay is younger");
-}
+    else
+    {
+        printf("hey, ajay is younger");
+    }
 
     return 0;
 }
