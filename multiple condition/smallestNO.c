629afa66b750d48b107ce59bbbfd4d3717a47198
vikash <vikashkelly@gmail.com>
2024-09-23 23:26:02 +0530

    finding smallest no.

---
+++ multiple condition/smallestNO.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main(){
+    int ram, shyam, ajay;
+    printf("Enter the age odf ram");
+    scanf("%d",&ram);
+
+    printf("Enter the age of shyam");
+    scanf("%d",&shyam);
+
+    printf("enter the age of ajay");
+    scanf("%d",&ajay);
+if (ram<shyam && ram<ajay)
+{
+   printf("hey, ram is younger");
+}
+if (shyam<ram && shyam<ajay)
+{
+    printf("hey, shyam is younger");
+}
+
+else
+{
+    printf("hey, ajay is younger");
+}
+
+    return 0;
+}
