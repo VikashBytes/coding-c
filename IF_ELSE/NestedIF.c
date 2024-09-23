2a50dcc481a4ee8c7da2f81ed84dce28854a1a65
vikash <vikashkelly@gmail.com>
2024-09-23 23:50:44 +0530

    nested if

--- IF_ELSE/NestedIF.c
+++ IF_ELSE/NestedIF.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
-int main(){
-int x;
-printf("enter your number");
-scanf("%d",&x);
+int main()
+{
+    int x;
+    printf("enter your number");
+    scanf("%d", &x);
 
-if (x%5==0)
-{
-  if (x%3==0)
-  {
-    printf("the number is divisible by 3 and 5 :");
-  }
-  
-}
-else
-{
-    printf("it is not divisible : ");
-}
+    if (x % 5 == 0)
+    {
+        if (x % 3 == 0)
+        {
+            printf("the number is divisible by 3 and 5 :");
+        }
+        else
+        {
+            printf("it is not divisible : ");
+        }
+    }
+    else
+    {
+        printf("it is not divisible : ");
+    }
 
     return 0;
 }
