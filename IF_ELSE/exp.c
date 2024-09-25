dbe78dec1c36710a0e3cf83eb503fdb9cb7a5e91
vikash <vikashkelly@gmail.com>
2024-09-25 19:19:18 +0530

    update

--- IF_ELSE/exp.c
+++ IF_ELSE/exp.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-void fun(){
+void fun()
+{
     printf("Vikash is chomu");
-    
 }
 int main()
 {
     int a;
     a = 128;
     a = a * a * a * 7 * a;
-  
-   fun();
+
+    fun();
 }
