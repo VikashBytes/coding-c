452e7d391d8811c5e4496d4f38fe4a4b2fe2d08a
vikash <vikashkelly@gmail.com>
2024-09-26 13:28:40 +0530

    ternary opeartor

---
+++ ternary operator/ternary.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter a number : ");
+    scanf("%d", &n);
+
+n % 2 == 0 ? printf("Even Number");
+    // if (n % 2 == 0)
+    //     printf("Even Number");
+    // else
+    //     printf("Odd Number");
+    return 0;
+}
