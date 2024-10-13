2346df45165df1a4bc3f4499d8d979062d7fbacf
vikash <vikashkelly@gmail.com>
2024-10-13 19:27:18 +0530

    Ques : Print all the even numbers from 1 to 100

---
+++ basic of loops/question_2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    // Ques : Print all the even numbers from 1 to 100
+    int i;
+for (int i = 1; i <=100; i++)
+{
+    if (i%2==0)
+    {
+        printf("%d\n",i);
+    }
+    
+}
+
+    return 0;
+}
