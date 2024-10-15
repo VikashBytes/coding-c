f93ee0c57ceaa8a3a0f917397ea69875affb29e2
vikash <vikashkelly@gmail.com>
2024-10-15 21:07:57 +0530

    update

--- basic of loops/AP3.c
+++ basic of loops/AP3.c
@@ -4,11 +4,11 @@ int n,a;
 // printf("Enter the number : ");
 // scanf("%d",&n);
 a = 100;
-printf("%d ",a);
 for (int i = 1;a>=3; i++)
 {
+    printf("%d ", a);
     a = a-3;
-    printf("%d ",a);
+    
 }
 
     return 0;
