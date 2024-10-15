8bb6710a6410d443bbca2601b394a94ff58137cf
vikash <vikashkelly@gmail.com>
2024-10-15 21:05:47 +0530

    Ques : Display this AP - 100,97,94,..upto all terms which are positive.

---
+++ basic of loops/AP3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+int n,a;
+// printf("Enter the number : ");
+// scanf("%d",&n);
+a = 100;
+printf("%d ",a);
+for (int i = 1;a>=3; i++)
+{
+    a = a-3;
+    printf("%d ",a);
+}
+
+    return 0;
+}
---
+++ basic of loops/tempCodeRunnerFile.c
@@ -0,0 +1,2 @@
+printf("Enter the number : ");
+// scanf("%d",&n);
