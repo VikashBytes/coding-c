d2b1932a623388aed285913192af1ef641e65d4d
vikash <vikashkelly@gmail.com>
2024-10-18 11:34:09 +0530

    hello world

---
+++ hello.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+// printf("Hello world\n");
+int a,b,c;
+printf("Enter the value of a :");
+scanf("%d",&a);
+printf("Enter the value of b : ");
+scanf("%d",&b);
+
+c = a + b;
+printf("The value of c = %d",c);
+    return 0;
+}
